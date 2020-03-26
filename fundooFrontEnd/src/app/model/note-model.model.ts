cdcfa1a9dc175ac82dd7f813d8945f76df8bb9dc
brijesh-biJJi <brijeshkanchan7@gmail.com>
2020-03-26 09:08:58 +0530

    [Brijesh] Add. Created NoteModel class

---
+++ fundooFrontEnd/src/app/model/note-model.model.ts
@@ -0,0 +1,12 @@
+export class NoteModel {
+    noteId:number;
+    title:string;
+    description:string;
+    isArchived:boolean;
+    isPinned:boolean;
+    isTrashed:boolean;
+    createdAt:string;
+    updatedAt:string;
+    colour:string;
+    reminder:Date;
+}
